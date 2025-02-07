7806ace82f34fc781a0a6a125e3d3b0225294b01
same-dust <2484193294@qq.com>
2025-02-07 23:36:48 +0800

    init

---
+++ Source/SlashWorld/Private/Items/Item.cpp
@@ -0,0 +1,19 @@
+#include "Items/Item.h"
+
+AItem::AItem()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AItem::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("AItem::BeginPlay"));
+}
+
+
+void AItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
